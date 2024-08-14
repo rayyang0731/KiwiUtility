663e3863e48281c287d3e76f1810760b825a4262
杨磊 <905527992@qq.com>
2024-08-14 11:06:44 +0800

    [修改] KiwiPath类调整函数参数

--- Packages/com.kiwi.utility/Runtime/Path/KiwiPath.cs
+++ Packages/com.kiwi.utility/Runtime/Path/KiwiPath.cs
@@ -13,20 +13,22 @@ namespace Kiwi.Utility
 		/// <summary>
 		/// 转换为相对路径
 		/// </summary>
-		/// <param name="rootPath">根路径(UNITY中一般传入 Application.dataPath)</param>
 		/// <param name="absolutePath">要转换的绝对路径</param>
+		/// <param name="rootPath">根路径(默认为 Application.dataPath)</param>
 		/// <returns>
 		/// 将 <paramref name="absolutePath"/> 转换为
 		/// 相对 <paramref name="rootPath"/> 的相对路径
 		/// </returns>
-		public static string ConvertToRelativePath(string rootPath, string absolutePath)
+		public static string ConvertToRelativePath(string absolutePath , string rootPath = null)
 		{
-			if (string.IsNullOrEmpty(rootPath) || string.IsNullOrEmpty(absolutePath))
+			if (string.IsNullOrEmpty(absolutePath))
 			{
-				Debug.LogError($"要转换的路径存在错误, rootPath : {rootPath}, absolutePath : {absolutePath}");
+				Debug.LogError($"要转换的路径存在错误 absolutePath : {absolutePath}");
 
 				return string.Empty;
 			}
+
+			if (string.IsNullOrEmpty(rootPath)) rootPath = Application.dataPath;
 
 			var uri         = new Uri(rootPath);
 			var absoluteUri = new Uri(absolutePath);
@@ -40,24 +42,26 @@ namespace Kiwi.Utility
 		/// <summary>
 		/// 转换为绝对路径
 		/// </summary>
-		/// <param name="rootPath">根路径(UNITY中一般传入 Application.dataPath)</param>
 		/// <param name="relativePath">要转换的相对路径</param>
+		/// <param name="rootPath">根路径(默认为 Application.dataPath)</param>
 		/// <returns>
 		/// 将 <paramref name="relativePath"/> 转换为
 		/// 相对 <paramref name="rootPath"/> 的绝对路径
 		/// </returns>
-		public static string ConvertToAbsolutePath(string rootPath, string relativePath)
+		public static string ConvertToAbsolutePath(string relativePath , string rootPath = null)
 		{
-			if (string.IsNullOrEmpty(rootPath) || string.IsNullOrEmpty(relativePath))
+			if (string.IsNullOrEmpty(relativePath))
 			{
 				Debug.LogError($"要转换的路径存在错误, rootPath : {rootPath}, relativePath : {relativePath}");
 
 				return string.Empty;
 			}
 
+			if (string.IsNullOrEmpty(rootPath)) rootPath = Application.dataPath;
+
 			var projectPath = Path.GetFullPath($"{rootPath}/..");
 
-			return Path.Combine(projectPath, relativePath);
+			return Path.Combine(projectPath , relativePath);
 		}
 	}
 }
